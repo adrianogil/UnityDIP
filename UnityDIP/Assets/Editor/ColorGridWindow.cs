fad2f60a619a3b085ea2c6c287c5442d4e899795
Adriano Gil <adrianogil.san@gmail.com>
2017-06-10 10:43:23 -0400

    Debug texture as a color grid

---
+++ UnityDIP/Assets/Editor/ColorGridWindow.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+// Simple script that creates a new dockable window
+public class ColorGridWindow : EditorWindow
+{
+    private IColorGrid colorGrid = null;
+
+    private const int maxInnerWidth = 600;
+    private const int maxInnerHeight = 600;
+
+    public void SetupColorGrid(IColorGrid colorGridInterface)
+    {
+        colorGrid = colorGridInterface;
+    }
+
+    [MenuItem("Debug/ColorGrid")]
+    static void Initialize()
+    {
+        Debug.Log("GilLog - ColorGridWindow::Initialize");
+
+        ColorGridWindow window  = (ColorGridWindow)CreateInstance<ColorGridWindow>();
+
+        IColorGrid colorGridInstance = DebugGrid.AddEditorWindow(window);
+
+        Debug.Log("GilLog - ColorGridWindow::Initialize - colorGridInstance " + colorGridInstance + " ");
+
+        if (colorGridInstance == null) return;
+
+        window.title = colorGridInstance.GetWindowTitle();
+
+        window.SetupColorGrid(colorGridInstance);
+
+        window.Show();
+    }
+
+    void OnGUI()
+    {
+        // Number of Cells
+        int cols = 15, rows = 15;
+
+        if (colorGrid != null)
+        {
+            cols = colorGrid.GetGridWidth();
+            rows = colorGrid.GetGridHeight();
+        }
+
+        float gridItemWidth = maxInnerWidth/(1.0f * cols);
+
+        GUI.Box(new Rect(5,5, 800, 800), "Colors");
+        GUILayout.BeginArea(new Rect(10, 10, 700, 700));
+        GUILayout.BeginVertical();
+        for (int y = 0; y < rows; y++)
+        {
+            GUILayout.BeginHorizontal();
+            for (int x = 0; x < cols; x++)
+            {
+                if (colorGrid != null) {
+                    EditorGUILayout.ColorField(GUIContent.none,
+                                               colorGrid.GetColor(x, y),
+                                               false, true, false, null, GUILayout.Width(gridItemWidth));
+                } else {
+                    EditorGUILayout.ColorField(GUIContent.none,
+                                               Color.Lerp(Color.red, Color.blue, (x + y * cols)/(1.0f * rows * cols)),
+                                               false, true, false, null, GUILayout.Width(gridItemWidth));
+                }
+
+            }
+            GUILayout.EndHorizontal();
+        }
+        GUILayout.EndVertical();
+        GUILayout.EndArea();
+    }
+}
---
+++ UnityDIP/Assets/Scripts/Debug/DebugGrid.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public interface IColorGrid
+{
+    string GetWindowTitle();
+    int GetGridWidth();
+    int GetGridHeight();
+    Color GetColor(int x, int y);
+}
+
+public enum GridType
+{
+    None,
+    FromMeshRenderer
+}
+
+public class DebugGrid : MonoBehaviour, IColorGrid {
+
+    public GridType gridType;
+
+    public GameObject targetObject;
+
+    public string windowTitle;
+
+    public int maxGridWidth = 80;
+    public int maxGridHeight = 80;
+
+    public Vector2 currentUV;
+    public int currentPixelCoordX, currentPixelCoordY;
+
+    private static List<DebugGrid> multipleInstances = new List<DebugGrid>();
+
+    #if UNITY_EDITOR
+    EditorWindow editorWindow = null;
+
+    public static DebugGrid AddEditorWindow(EditorWindow window)
+    {
+        for (int i = 0; i < multipleInstances.Count; i++)
+        {
+            if (multipleInstances[i] != null && multipleInstances[i].editorWindow == null)
+            {
+                multipleInstances[i].SetupEditorWindow(window);
+                return multipleInstances[i];
+            }
+        }
+
+        return null;
+    }
+
+    public void SetupEditorWindow(EditorWindow editor)
+    {
+        editorWindow = editor;
+    }
+
+    #endif
+
+    public string GetWindowTitle()
+    {
+        return windowTitle;
+    }
+
+    public int GetGridWidth() {
+        return maxGridWidth;
+    }
+
+    public int GetGridHeight() {
+        return maxGridHeight;
+    }
+
+    public Color GetColor(int x, int y) {
+        if (gridType == GridType.FromMeshRenderer && targetObject != null)
+        {
+            Texture2D tex = (Texture2D) (targetObject.GetComponent<MeshRenderer>().material.GetTexture("_MainTex"));
+
+            if (tex == null) return Color.white;
+
+            currentPixelCoordX = (int)(currentUV.x * tex.width);
+            currentPixelCoordY = (int) (currentUV.y * tex.height);
+
+            x -= maxGridWidth / 2;
+            y -= maxGridHeight / 2;
+
+            y *= -1;
+
+            x = (tex.width + currentPixelCoordX + x) % tex.width;
+            y = (tex.height + currentPixelCoordY + y) % tex.height;
+
+            return tex.GetPixel(x, y);
+        }
+
+        return Color.white;
+    }
+
+    void Awake()
+    {
+        multipleInstances.Add(this);
+    }
+
+    void Start()
+    {
+        currentUV = Vector2.zero;
+    }
+
+    void Update()
+    {
+        if (gridType == GridType.None) return;
+
+        if (gridType == GridType.FromMeshRenderer && targetObject != null && Input.GetMouseButton(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (!targetObject.GetComponent<MeshCollider>().Raycast(ray, out hit, 100.0F))
+                return;
+
+            currentUV = hit.textureCoord;
+        }
+
+        #if UNITY_EDITOR
+        if (editorWindow != null)
+        {
+            editorWindow.Repaint();
+        }
+        #endif
+    }
+}
