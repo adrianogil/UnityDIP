75ec9ba3a7e1ba2f04178a5b4847a520826ae985
Adriano Gil <adrianogil.san@gmail.com>
2017-06-15 16:36:59 -0400

    WIP Convolutional Demo

---
+++ UnityDIP/Assets/Scripts/ConvolutionFilter.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public enum ConvFilterType
+{
+    CustomFilter
+}
+
+public class ConvolutionFilter : MonoBehaviour {
+
+    
+    public ConvFilterType filterType;
+
+    [HideInInspector]
+    public float[] kernelweights = new float[9];
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public float[] GetCurrentKernelWeights()
+    {
+        return kernelweights;
+    }
+
+    public void ApplyFilter()
+    {
+        
+        Material material = GetComponent<MeshRenderer>().sharedMaterial;
+        // GetComponent<MeshRenderer>().sharedMaterial.SetFloatArray("_Kernel", GetCurrentKernelWeights());
+        float[] weights = GetCurrentKernelWeights();
+        
+        Debug.Log("GilLog - ConvolutionFilter::ApplyFilter - weights.Length " + weights.Length);
+
+        material.SetFloatArray("_Kernel", weights);
+        material.SetFloat("_KernelLength", weights.Length);
+        
+        // Shader.SetGlobalFloatArray("_Kernel", GetCurrentKernelWeights());   
+    }
+}
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(ConvolutionFilter))]
+public class ConvolutionFilterEditor : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        ConvolutionFilter filter = target as ConvolutionFilter;
+
+        if (filter == null) return;
+
+
+        float gridItemWidth = 50f;
+        int rows = 3;
+        int cols = 3;
+
+        float[] kernel = filter.GetCurrentKernelWeights();
+
+        float[] kernelValues = new float[kernel.Length];
+        for (int i = 0; i < kernelValues.Length; i++)
+            kernelValues[i] = kernel[i];
+
+        GUILayout.BeginVertical();
+        for (int y = 0; y < rows; y++)
+        {
+            GUILayout.BeginHorizontal();
+            for (int x = 0; x < cols; x++)
+            {
+                kernelValues[x + y*cols] = EditorGUILayout.FloatField(GUIContent.none,
+                                               kernelValues[x + y*cols], GUILayout.Width(gridItemWidth));
+            }
+            GUILayout.EndHorizontal();
+        }
+        GUILayout.EndVertical();
+
+        if (filter.filterType == ConvFilterType.CustomFilter)
+        {
+            filter.kernelweights = kernelValues;
+        }
+
+        if (GUILayout.Button("Apply filter"))
+        {
+            filter.ApplyFilter();
+        }
+    }
+
+}
+#endif
---
+++ UnityDIP/Assets/Shaders/ConvFilter.shader
@@ -0,0 +1,64 @@
+Shader "DIP/ConvolutionalFilter"
+{
+    Properties {
+        _MainTex ("Image", 2D) = "white"
+    }
+    Subshader {
+        Pass {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            uniform int _KernelLength = 9;
+            uniform float _Kernel[9];
+
+            sampler _MainTex;
+            float4 _MainTex_TexelSize;
+
+            struct vert_input {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct vert_output {
+                float4 vertex : SV_POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            vert_output vert(vert_input i)
+            {
+                vert_output o;
+
+                o.vertex = UnityObjectToClipPos(i.vertex);
+                o.uv = i.uv;
+
+                return o;
+            }
+
+            half4 frag(vert_output o) : COLOR {
+
+                half4 texColors[9];
+                #define GRABPIXEL(px,py) tex2D( _MainTex, o.uv + float2(px * _MainTex_TexelSize.x, py * _MainTex_TexelSize.y))
+
+                texColors[0] = _Kernel[0] * GRABPIXEL(-1,-1);
+                texColors[1] = _Kernel[1] * GRABPIXEL(-1, 0);
+                texColors[2] = _Kernel[2] * GRABPIXEL(-1, 1);
+                texColors[3] = _Kernel[3] * GRABPIXEL( 0,-1);
+                texColors[4] = _Kernel[4] * GRABPIXEL( 0, 0);
+                texColors[5] = _Kernel[5] * GRABPIXEL( 0, 1);
+                texColors[6] = _Kernel[6] * GRABPIXEL( 1,-1);
+                texColors[7] = _Kernel[7] * GRABPIXEL( 1, 0);
+                texColors[8] = _Kernel[8] * GRABPIXEL( 1, 1);
+
+                // return texColors[0];
+
+                // return float4(0,0,0,1);
+
+                return (texColors[0] + texColors[1] + texColors[2] + texColors[3] + texColors[4] + 
+                       texColors[5] + texColors[6] + texColors[7] + texColors[8]);
+            }
+
+            ENDCG
+        }   
+    }
+}
